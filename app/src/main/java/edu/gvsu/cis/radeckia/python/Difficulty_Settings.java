6b64b7aabb3b5d22ab54cba1d9c739f0bf7445a6
Alex Radecki <alex_radecki@yahoo.com>
2016-04-19 19:48:47 -0400

    Changed fab to easy button

--- app/src/main/java/edu/gvsu/cis/radeckia/python/Difficulty_Settings.java
+++ app/src/main/java/edu/gvsu/cis/radeckia/python/Difficulty_Settings.java
@@ -21,8 +21,8 @@ public class Difficulty_Settings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        easy = (Button) findViewById(R.id.fab);
+        easy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
