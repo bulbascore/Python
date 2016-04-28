82aebc532803c5b51c50db93b979ec687f3f4fd4
Alex Radecki <alex_radecki@yahoo.com>
2016-04-27 21:25:14 -0400

    Added welcome and results activity

--- app/src/main/java/edu/gvsu/cis/radeckia/python/MainActivity.java
+++ app/src/main/java/edu/gvsu/cis/radeckia/python/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.gvsu.cis.radeckia.python;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,30 +11,45 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import java.util.ArrayList;
 import java.util.Date;
-import com.google.android.gms.*;
+
 import com.google.android.gms.games.Games;
+import edu.gvsu.cis.radeckia.python.Main;
+import edu.gvsu.cis.radeckia.python.Disks;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener{
 
-    private int size = 20;
+    private int gameCols = 7;
+    private int gameRows = 6;
     private GameLogic gameLogic;
     private long reset = 0;
     private GoogleApiClient mClient;
+    private static int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    //private boolean mAutoStartSignInFlow = true;
+    private boolean mSignInClicked = false;
+    private int cols = 7;
+    private int rows = 6;
+    private Disks disks[][] = new Disks[cols][rows];
+    private Player player1;
+    private Player player2;
+    private boolean gameDone = false;
 
-    private void reset() {
+    /*private void reset() {
         Date increment = new Date();
         reset = increment.getTime();
         if(reset == 100) {
@@ -39,8 +57,215 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             increment.setTime(0);
             reset = 0;
         }
+    }*/
+
+    public void placePiece(String Player, int col){
+        int i = col;
+
+        if(legalMove(i)){
+            for (int k = disks[i].length - 1; k > 0; k++) {
+                if (disks[k][i] == null){
+                    Disks d = new Disks (Player, k, i);
+                    disks[k][i] = d;
+
+                    if(hasWon(k, i)){
+                        gameDone =  true;
+                        endGame();
+                    }
+                    //TODO add more work to update the UI
+                }
+            }
+        }
+
     }
 
+    //checks if the move is possible
+    public boolean legalMove(int i){
+        if (gameDone == false) {
+            if (disks[0][i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void endGame(){
+        Intent launchme = new Intent(MainActivity.this, ResultsScreen.class);
+        launchme.putExtra("winner", "Easy");
+        startActivity(launchme);
+    }
+
+    //returns how many pieces there are in a row.
+    public boolean hasWon(int r, int c){
+
+        int i = 1;
+        int t = 1;
+        String s = disks[r][c].getPlayer();
+
+        //checks horizontal options
+        if (disks[r+1][c].getPlayer().equals(s)&& r+1 < disks.length){
+            i++;
+            if(disks[r+2][c].getPlayer().equals(s) && r+2 < disks.length){
+                i++;
+                if(disks[r+3][c].getPlayer().equals(s)&& r+3 < disks.length){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+            if(disks[r-1][c].getPlayer().equals(s) && r-1 >=0){
+                i++;
+                if (i == 4){
+                    return true;
+                }
+                if(disks[r-2][c].getPlayer().equals(s) && r-2 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+
+                }
+
+            }
+        }
+        if(disks[r-1][c].getPlayer().equals(s) && r-1 >=0){
+            i++;
+            if(disks[r-2][c].getPlayer().equals(s) && r-2 >= 0){
+                i++;
+                if(disks[r-3][c].getPlayer().equals(s)&& r-3 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //checks for diaginal left
+        if (disks[r+1][c+1].getPlayer().equals(s)&& r+1 < disks.length && c+1 < disks[r].length){
+            i++;
+            if (disks[r+2][c+2].getPlayer().equals(s)&& r+2 < disks.length && c+2 < disks[r].length){
+                i++;
+                if (disks[r+3][c+3].getPlayer().equals(s)&& r+3 < disks.length && c+3 < disks[r].length){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+            if(disks[r-1][c-i].getPlayer().equals(s) && r-1 >=0){
+                i++;
+                if (i == 4){
+                    return true;
+                }
+                if(disks[r-2][c-2].getPlayer().equals(s) && r-2 >=0 && c-i >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+
+                }
+
+            }
+        }
+        if(disks[r-1][c-1].getPlayer().equals(s) && r-1 >=0 && c-1 >=0){
+            i++;
+            if(disks[r-2][c-2].getPlayer().equals(s) && r-2 >=0 && c-2 >=0){
+                i++;
+                if(disks[r-3][c-3].getPlayer().equals(s) && r-3 >=0 && c-3 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //checks for diaginal right
+        if (disks[r+1][c-1].getPlayer().equals(s)&& r+1 < disks.length && c+1 >=0){
+            i++;
+            if (disks[r+2][c-2].getPlayer().equals(s)&& r+2 < disks.length && c+2 >=0){
+                i++;
+                if (disks[r+3][c-3].getPlayer().equals(s)&& r+3 < disks.length && c+3 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+            if(disks[r-1][c+i].getPlayer().equals(s) && r-1 >=0 && c+1 < disks[r].length){
+                i++;
+                if (i == 4){
+                    return true;
+                }
+                if(disks[r-2][c+2].getPlayer().equals(s) && r-2 >=0 && c+2 < disks[r].length){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+
+                }
+
+            }
+        }
+        if(disks[r-1][c+1].getPlayer().equals(s) && r+1 >=0 && c+1 < disks[r].length){
+            i++;
+            if(disks[r-2][c+2].getPlayer().equals(s) && r+2 >=0 && c+2 < disks[r].length){
+                i++;
+                if(disks[r-3][c+3].getPlayer().equals(s) && r+3 >=0 && c+3 < disks[r].length){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //checks for vertical
+        if (disks[r][c+1].getPlayer().equals(s)&& c+1 < disks.length){
+            i++;
+            if(disks[r][c+2].getPlayer().equals(s) && c+2 < disks.length){
+                i++;
+                if(disks[r][c+3].getPlayer().equals(s)&& c+3 < disks.length){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+            if(disks[r][c-1].getPlayer().equals(s) && c-1 >=0){
+                i++;
+                if (i == 4){
+                    return true;
+                }
+                if(disks[r-2][c].getPlayer().equals(s) && c-2 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+
+                }
+
+            }
+        }
+        if(disks[r][c-1].getPlayer().equals(s) && c-1>=0){
+            i++;
+            if(disks[r][c-2].getPlayer().equals(s) && c-2 >= 0){
+                i++;
+                if(disks[r][c-3].getPlayer().equals(s)&& c-3 >=0){
+                    i++;
+                    if (i == 4){
+                        return true;
+                    }
+                }
+            }
+        }
+
+
+
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +282,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .build();
 
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout);
-        Drawable border = getResources().getDrawable(R.drawable.tile);
-        Drawable snake = getResources().getDrawable(R.drawable.snake);
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
+        Drawable border = getResources().getDrawable(R.drawable.tile_empty);
+        Drawable p1_tile = getResources().getDrawable(R.drawable.p1_tile_occupied);
+        Drawable p2_tile = getResources().getDrawable(R.drawable.p2_tile_occupied);
+        Button one = (Button) findViewById(R.id.one);
+        Button two = (Button) findViewById(R.id.two);
+        Button three = (Button) findViewById(R.id.three);
+        Button four = (Button) findViewById(R.id.four);
+        Button five = (Button) findViewById(R.id.five);
+        Button six = (Button) findViewById(R.id.six);
+        Button seven = (Button) findViewById(R.id.seven);
+
+        for(int i = 0; i < gameCols; i++) {
+            for(int j = 0; j < gameRows; j++) {
                 //ArrayList<Cell> tiles = gameLogic.getNonEmptyTiles();
 
                 int ri = j;
@@ -68,7 +302,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
                 TextView myText = new TextView(this);
 
-                for(int k = 0; k < size; k++) {
+                //for(int k = 0; k < size; k++) {
                     //tiles.size()
 
                     /*if(tiles.get(k).column != j && tiles.get(k).row == i) {
@@ -78,22 +312,32 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                         myText.setText("" + tiles.get(k).value);
                         tiles.remove(k);
                     }*/
-                }
+                //}
                 myText.setTextSize(25);
 
                 //if()
                 myText.setBackground(border);
                 myText.setGravity(Gravity.CENTER_HORIZONTAL);
-                myText.setHeight(49);
-                myText.setWidth(49);
+                myText.setHeight(139);
+                myText.setWidth(139);
                 /* place the TextView at the desired row and column */
                 GridLayout.Spec r_spec = GridLayout.spec(ri, GridLayout.CENTER);
                 GridLayout.Spec c_spec = GridLayout.spec(ci, GridLayout.CENTER);
                 GridLayout.LayoutParams par = new GridLayout.LayoutParams(r_spec, c_spec);
                 par.setGravity(Gravity.CENTER_HORIZONTAL);
                 grid.addView(myText, par);
-            }
 
+                for(int m = 0; m < cols-1; m++) {
+                    for(int n = 0; n < rows-1; n++) {
+                        if(disks[m][n].getPlayer() == player1.name()) {
+                            myText.setBackground(p1_tile);
+                        }
+                        else if(disks[m][n].getPlayer() == player2.name()) {
+                            myText.setBackground(p2_tile);
+                        }
+                    }
+                }
+            }
         }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -104,7 +348,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                         .setAction("Action", null).show();
             }
         });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placePiece();
+            }
+        });
     }
+
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+        mClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mClient.disconnect();
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -130,16 +393,92 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        //String name = Games.getCurrentAccountName(mClient);
+        //Log.d(TAG, "onConnected: " + name + " " + mClient.isConnected());
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        mClient.connect();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            // already resolving
+            return;
+        }
 
+        // if the sign-in button was clicked or if auto sign-in is enabled,
+        // launch the sign-in flow
+        if (mSignInClicked /*|| mAutoStartSignInFlow*/) {
+            //mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+
+            // Attempt to resolve the connection failure using BaseGameUtils.
+            // The R.string.signin_other_error value should reference a generic
+            // error string in your strings.xml file, such as "There was
+            // an issue with sign-in, please try again later."
+
+            if(mSignInClicked) {
+                mSignInClicked = false;
+                mResolvingConnectionFailure = true;
+            }
+
+            if(connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mResolvingConnectionFailure = true;
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                    mResolvingConnectionFailure = false;
+                    mClient.connect();
+                }
+            }
+            /*else {
+                Snackbar.make(this, connectionResult.getErrorMessage(), Snackbar.LENGTH_LONG);
+            }
+
+           if (!asd.resolveConnectionFailure(this,
+                    mClient, connectionResult,
+                    RC_SIGN_IN, R.string.on_signIn_failed)) {
+                mResolvingConnectionFailure = false;
+            }*/
+        }
+
+        // Put code here to display the sign-in button
     }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mClient.connect();
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+
+
+                //BaseGameUtils.showActivityResultError(this,
+                //        requestCode, resultCode, R.string.on_signIn_failed);
+            }
+        }
+    }
+
+    /*@Override
+    public void onSignInPressed() {
+        mSignInClicked = true;
+        SharedPreferences pref = PreferenceManager;
+        mClient.connect();
+    }
+
+    @Override
+    public void onSignOutPressed() {
+        mSignInClicked = false;
+    }*/
 }
